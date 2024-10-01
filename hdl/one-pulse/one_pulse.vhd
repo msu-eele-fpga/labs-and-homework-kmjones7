94a3f4d75a874971ea79c3151d87257a6cfbf779
kmjones7 <kaleb.m.jones@outlook.com>
2024-09-30 22:32:12 -0600

    hw 6 commit

--- hdl/one-pulse/one_pulse.vhd
+++ hdl/one-pulse/one_pulse.vhd
@@ -1,1 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
+entity one_pulse is
+  port(
+    clk : in std_ulogic;
+    rst : in std_ulogic;
+    input : in std_ulogic;
+    pulse : out std_ulogic
+    );
+end entity one_pulse;
+
+architecture one_pulse_arch of one_pulse is
+  signal PULSED : integer := 0;
+  begin
+  PULSE_PROCESS : process (clk, input)
+    begin
+    if(rising_edge(clk)) then
+      if (input = '1') then -- button pushed
+        if (PULSED = 0) then -- one pulse HASN'T happened
+          pulse <= '1'; -- pulse
+          PULSED <= 1;
+        else           -- one pulse HAS happened
+          pulse <= '0';
+        end if;
+      else
+        PULSED <= 0;
+      end if;
+    end if;
+  end process;
+   
+end architecture one_pulse_arch;
